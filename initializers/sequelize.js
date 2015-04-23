f98c4bd2ba6d7542c6a046ef45006809d0fefe60
Victor Heng <bakavic@gmail.com>
2015-04-23 12:38:51 +0800

    test with bluebird.

--- initializers/sequelize.js
+++ initializers/sequelize.js
@@ -1,6 +1,7 @@
 var path              = require('path');
 var fs                = require('fs');
 var Sequelize         = require('sequelize');
+var Promise           = require('bluebird');
 
 module.exports = {
   initialize: function(api, next){
@@ -28,58 +29,51 @@ module.exports = {
         this.migrate({ method: 'down' }, next);
       },
 
-      connect: function(next){
-        api.sequelize.sequelize = new Sequelize(
-          api.config.sequelize.database,
-          api.config.sequelize.username,
-          api.config.sequelize.password,
-          api.config.sequelize
-        );
+      connect: function() {
+        return new Promise(function (resolve, reject) {
+          api.sequelize.sequelize = new Sequelize(
+              api.config.sequelize.database,
+              api.config.sequelize.username,
+              api.config.sequelize.password,
+              api.config.sequelize
+          );
 
-        var dir = path.normalize(api.projectRoot + '/models');
-        fs.readdirSync(dir).forEach(function(file){
-          var nameParts = file.split("/");
-          var name = nameParts[(nameParts.length - 1)].split(".")[0];
-          api.models[name] = api.sequelize.sequelize.import(dir + '/' + file);
-        });
-
-        api.sequelize.test(next);
+          var dir = path.normalize(api.projectRoot + '/models');
+          fs.readdirSync(dir).forEach(function (file) {
+              var nameParts = file.split("/");
+              var name = nameParts[(nameParts.length - 1)].split(".")[0];
+              api.models[name] = api.sequelize.sequelize.import(dir + '/' + file);
+          });
+          resolve();
+        }).then(api.sequelize.test);
       },
 
-      loadFixtures: function(next) {
-          if(api.env === "test") {
-            var SequelizeFixtures = require('sequelize-fixtures');
-            SequelizeFixtures.loadFile(api.projectRoot + '/test/fixtures/*.{json,yml,js}', api.models, function () {
-              next();
-            });
-          } else {
-            next();
-          }
+      loadFixtures: function() {
+          return new Promise(function (resolve, reject) {
+              var SequelizeFixtures = require('sequelize-fixtures');
+              SequelizeFixtures.loadFile(api.projectRoot + '/test/fixtures/*.{json,yml,js}', api.models, resolve);
+          })
       },
 
       autoMigrate: function(next) {
-        if(api.config.sequelize.autoMigrate) {
-            api.sequelize.migrate({method: 'up'}, function () {
-              next();
-            });
-        } else {
-            next();
-        }
+          return new Promise(function (resolve, reject) {
+              api.sequelize.migrate({method: 'up'}, resolve);
+          });
       },
 
-      // api.sequelize.test([exitOnError=true], next);
       // Checks to see if mysql can be reached by selecting the current time
       // Arguments:
       //  - next (callback function(err)): Will be called after the test is complete
       //      If the test fails, the `err` argument will contain the error
-      test: function(next){
-        api.sequelize.sequelize.query("SELECT NOW()").then(function(){
-          next();
-        }).catch(function(err){
-          api.log(err, 'warning');
-          console.log(err);
-          next(err);
-        });
+      test: function() {
+          return new Promise(function (resolve, reject) {
+              api.sequelize.sequelize.query("SELECT NOW()").then(resolve)
+              .catch(function (err) {
+                  api.log(err, 'warning');
+                  console.log(err);
+                  reject(err);
+              });
+          });
       }
     };
 
@@ -88,14 +82,19 @@ module.exports = {
 
   startPriority: 1001, // the lowest post-core middleware priority
   start: function(api, next){
-    api.sequelize.connect(function(err){
-      if(err) {
-        return next(err);
-      }
-
-      api.sequelize.autoMigrate(function() {
-          api.sequelize.loadFixtures(next);
-      });
-    });
+      api.sequelize.connect().then(function () {
+          if (api.config.sequelize.autoMigrate) {
+              return api.sequelize.autoMigrate();
+          }
+      }).then(function () {
+          if(api.env === "test") {
+              return api.sequelize.loadFixtures();
+          }
+      }).then(next)
+          .catch(function (err) {
+              api.log(err, 'warning');
+              console.log(err);
+              next(err);
+          });
   }
 };
